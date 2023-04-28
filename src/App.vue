b0f744b7d65bc8074dce5d6188d4e3d0c865e482
devjaiye <dayojaiye74@gmail.com>
2023-04-28 13:07:04 +0100

    getters

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,32 @@
       <h1>Todo</h1>
     </header>
 
+    <!-- filter toggle -->
+    <nav class="filter">
+       <button @click="filter = 'all'">All Tasks</button>
+       <button @click="filter = 'favs'">Favorite Tasks</button>
+    </nav>
+
     <!-- Task list items -->
-    <div class="task-list">
+    <div class="task-list" v-if="filter === 'all'">
+        <p> You have {{taskStore.totalCount}} task(s) left</p>
         <div v-for="task in taskStore.tasks">
           <TaskDetails :task="task"/>
-          
         </div>
+    </div>
+
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>You have {{ taskStore.favCount }} favorite to do</p>
+      <div v-for="task in taskStore.favs">
+        <TaskDetails :task="task" />
+      </div>
     </div>
 
   </main>
 </template>
 
 <script>
+  import {ref} from 'vue'
   import {useTaskStore} from './stores/TaskStore.js'
   import TaskDetails from './components/TaskDetails.vue'
 
@@ -26,7 +40,8 @@
     components: {TaskDetails},
     setup(){
       const taskStore = useTaskStore()
-      return {taskStore}
+      const filter = ref('all')
+      return {taskStore, filter}
     }
   }
 </script>
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -7,5 +7,20 @@ import { defineStore } from "pinia";
             {id: 2, title: "Concert", isFav: false }
         ], 
     }), 
-     
+
+    //.. getters
+    getters: {
+        //...store favorite tasks
+        favs(){
+            return this.tasks.filter(savedTasks => savedTasks.isFav)
+        },
+        favCount(){
+            return this.tasks.reduce((prev, current)=> {
+                return current.isFav ? prev + 1 : prev
+            }, 0)
+        }, 
+        totalCount: (state) => {
+            return state.tasks.length
+        }, 
+    } 
   })
